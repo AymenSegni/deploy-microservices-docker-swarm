061b24e820887ae0f1c5dfeff7697a648deed9df
Aymen Segni <segniaymen1@gmail.com>
2018-10-18 18:02:46 +0000

    setup-docker.sh

--- scripts/setup-docker.sh
+++ scripts/setup-docker.sh
@@ -17,3 +17,4 @@ sudo docker run hello-world
 sudo groupadd docker
 sudo usermod -aG docker $USER
 sudo systemctl enable docker
+base=https://github.com/docker/machine/releases/download/v0.14.0 && curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine && sudo install /tmp/docker-machine /usr/local/bin/docker-machine
